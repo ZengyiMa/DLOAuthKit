762a3cf2cc35859c0d3aa9aa1f5489aca1b5e0db
famulei <mazengyi@famulei.com>
2016-09-01 15:21:46 +0800

    Initial Commit

---
+++ DLOAuthKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DLOAuthKit
+//
+//  Created by famulei on 9/1/16.
+//  Copyright © 2016 famulei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
